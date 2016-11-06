13601b647f6b397128457af9abcc49441affe8e6
Daniel Kwolek <Daniel@Daniels-MacBook-Pro-2.local>
2016-11-06 15:43:49 -0500

    /hello endpoint added

---
+++ Synesthetica-Service/Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "Synesthetica-Service",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1)
+    ],
+    exclude: [
+        "Config",
+        "Database",
+        "Localization",
+        "Public",
+        "Resources",
+        "Tests",
+    ]
+)
+
